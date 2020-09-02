74fd3affbe8a7f55ea09340bc163840ffd203503
Harsh-2420 <harsh2420@gmail.com>
2020-09-02 19:09:25 -0400

    Added final pictures. CSS not done

--- src/Components/Keywords.js
+++ src/Components/Keywords.js
@@ -1,16 +1,28 @@
-import React from "react"
-import "../App.css"
+import React from "react";
+import "../App.css";
+import Home from "../Images/home.png";
+import Match from "../Images/match.png";
+import AddInfo from "../Images/add_info.png";
+import Chat from "../Images/chat.png";
 
-function Keywords(){
-    return(
-        <div className="keywords" id="Keywords">
-            <div className="keywords-text">Keywords</div>
-            <div className="keywords-caps">choose from a thousand of trendy keywords or make your own!</div>
-
-            <div className="keywords-placeholder">Keywords</div>
-            <div className="keywords-caps-placeholder">choose from a thousand of trendy keywords or make your own!</div>
+function Keywords() {
+  return (
+    <div className="keywords" id="Keywords">
+      <div className="keywords-text">keywords</div>
+      <div className="keywords-caps">
+        choose from a thousand of trendy keywords or make your own!
       </div>
-    )
+      <img src={Home} className="home" />
+      <img src={Match} className="match" />
+
+      <div className="keywords-placeholder">keywords</div>
+      <div className="keywords-caps-placeholder">
+        choose from a thousand of trendy keywords or make your own!
+      </div>
+      <img src={AddInfo} className="addinfo" />
+      <img src={Chat} className="chat" />
+    </div>
+  );
 }
 
 export default Keywords;
--- src/Components/WhyFindr.js
+++ src/Components/WhyFindr.js
@@ -1,33 +1,39 @@
-import React from "react"
-import "../App.css"
-import Placeholder from "../Images/placeholder.jpg";
+import React from "react";
+import "../App.css";
+import Icon1 from "../Images/icon1.png";
+import Icon2 from "../Images/icon2.png";
+import Icon3 from "../Images/icon3.png";
 
-function WhyFindr(){
-    return(
-     <div className="why-findr">
-          <p className="why-findr-title">why Findr?</p>
+function WhyFindr() {
+  return (
+    <div className="why-findr">
+      <p className="why-findr-title">why Findr?</p>
 
-          <div>
-            <img src={Placeholder} className="pic1"/>
-            <figcaption className="pic1cap">don't stop swiping</figcaption>
-            <figcaption className="pic1capBottom">find partners for your courses,
-            projects, research. Take your pick!</figcaption>
-          </div>
-
-          <div>
-            <img src={Placeholder} className="pic2"/>
-            <figcaption className="pic2cap">network smarter</figcaption>
-            <figcaption className="pic2capBottom">find peers from different fields
-            and expand your network!</figcaption>
-          </div>
-
-          <div>
-            <img src={Placeholder} className="pic3"/>
-            <figcaption className="pic3cap">learn with others</figcaption>
-            <figcaption className="pic3capBottom">match with people and more!</figcaption>
-          </div> 
+      <div>
+        <img src={Icon1} className="pic1" />
+        <figcaption className="pic1cap">don't stop swiping</figcaption>
+        <figcaption className="pic1capBottom">
+          find partners for your courses, projects, research. Take your pick!
+        </figcaption>
       </div>
-    )
+
+      <div>
+        <img src={Icon2} className="pic2" />
+        <figcaption className="pic2cap">network smarter</figcaption>
+        <figcaption className="pic2capBottom">
+          find peers from different fields and expand your network!
+        </figcaption>
+      </div>
+
+      <div>
+        <img src={Icon3} className="pic3" />
+        <figcaption className="pic3cap">learn with others</figcaption>
+        <figcaption className="pic3capBottom">
+          match with people and more!
+        </figcaption>
+      </div>
+    </div>
+  );
 }
 
 export default WhyFindr;
