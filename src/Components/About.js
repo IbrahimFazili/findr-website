64e0c1264883fc630c25ac84e6aa581bf13cc83a
Harsh-2420 <harsh2420@gmail.com>
2020-09-03 19:31:59 -0400

    Set Up About Us bar

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,41 +5,61 @@ import { Link } from "react-router-dom";
 function About() {
   return (
     <div className="bottom-header">
-      <div style={{ display: "inline-block", width: "30% " }}>
-        <p className="launch">Launching soon!</p>
+      <div
+        style={{
+          display: "inline-block",
+          width: "30%",
+          float: "left",
+          verticalAlign: "top",
+          marginTop: "10%",
+        }}
+      >
+        <p className="launch">Launching Soon</p>
       </div>
-      <div>
-        <p
-          className="AboutUs"
-          style={{ display: "inline-block", width: "30%" }}
-        >
-          About Us
+
+      <div
+        style={{
+          display: "inline-block",
+          width: "30%",
+          verticalAlign: "top",
+          marginTop: "2.5%",
+        }}
+      >
+        <p style={{ fontSize: "larger" }}>
+          <p style={{ fontSize: "x-large" }}>About Us</p>
           <br></br>
-          Findr is a platform to help you grow and find the perfect partner -
+          Findr is a platform to help you grow and find the perfect partner
+          <br></br>-<br></br>
           No, dummy, not a date! Partner for your courses, projects, startups,
           anything!
         </p>
       </div>
-      <div className="links" style={{ display: "inline-block", width: "30%" }}>
+      <div
+        className="links"
+        style={{
+          display: "inline-block",
+          width: "30%",
+          verticalAlign: "top",
+          marginRight: "5%",
+        }}
+      >
         <ul>
-          <div style={{ width: "80%" }}>
-            <li class="nav-link">
-              <Link to="/Privacy">Privacy Policy</Link>
-              <div class="underline"></div>
-            </li>
-            <li class="nav-link">
-              <Link to="/TermsAndConditions">Terms and Conditions</Link>
-              <div class="underline"></div>
-            </li>
-            <li class="nav-link" style={{ marginRight: "2%" }}>
-              <Link to="/Safety">Safety Tips</Link>
-              <div class="underline"></div>
-            </li>
-            <li class="nav-link" style={{ marginRight: "2%" }}>
-              <Link to="/Guidelines">Community Guidlines</Link>
-              <div class="underline"></div>
-            </li>
-          </div>
+          <li class="nav-link">
+            <Link to="/Privacy">Privacy Policy</Link>
+            <div class="underline"></div>
+          </li>
+          <li class="nav-link">
+            <Link to="/TermsAndConditions">Terms and Conditions</Link>
+            <div class="underline"></div>
+          </li>
+          <li class="nav-link">
+            <Link to="/Safety">Safety Tips</Link>
+            <div class="underline"></div>
+          </li>
+          <li class="nav-link">
+            <Link to="/Guidelines">Community Guidlines</Link>
+            <div class="underline"></div>
+          </li>
         </ul>
       </div>
     </div>
