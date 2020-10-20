005470b1b09933e56209e364fbb43d2b45e09f84
Harsh-2420 <harsh2420@gmail.com>
2020-10-20 19:52:55 -0400

    Navbar bug fixes

--- src/Components/Nav-Bar.js
+++ src/Components/Nav-Bar.js
@@ -2,10 +2,10 @@ import React from "react";
 import "../App.css";
 // import navLogo from "../Images/Findr_logo_g.svg";
 import { Link } from "react-router-dom";
-import Instagram from "../Images/instagram.svg";
-import LinkedIn from "../Images/linkedin-round-color.svg";
-import Facebook from "../Images/facebook.svg";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import Instagram from "../Images/instagram_findr.svg";
+import LinkedIn from "../Images/linkedin.png";
+import Facebook from "../Images/facebook_new.svg";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import styled from "styled-components";
 import navLogo from "../Images/Findr-logo-white.svg";
 
@@ -45,24 +45,11 @@ export const NavBar = () => (
               <div class="underline"></div>
             </Nav.Link>
           </Nav.Item>
-          <Nav.Item className="nav-icon-wrapper">
-            <a href="https://www.instagram.com/findr.study/" target="_blank">
-              <img src={Instagram} className="social-media-logo" />
-            </a>
-          </Nav.Item>
-          <Nav.Item className="nav-icon-wrapper">
-            <a href="https://www.facebook.com/findr.study.3" target="_blank">
-              <img src={Facebook} className="social-media-logo" />
-            </a>
-          </Nav.Item>
-          <Nav.Item className="nav-icon-wrapper">
-            <a
-              href="https://www.linkedin.com/company/findrapp/"
-              target="_blank"
-            >
-              <img src={LinkedIn} className="social-media-logo" />
-            </a>
-          </Nav.Item>
+          <NavDropdown title="social" id="basic-nav-dropdown">
+            <NavDropdown.Item href="https://www.instagram.com/findr.study/" target="_blank"><img src={Instagram} className="insta-logo" /></NavDropdown.Item>
+            <NavDropdown.Item href="https://www.facebook.com/findr.study.3" target="_blank"><img src={Facebook} className="fb-logo" /></NavDropdown.Item>
+            <NavDropdown.Item href="https://www.linkedin.com/company/findrapp/" target="_blank"><img src={LinkedIn} className="linkedin-logo" /></NavDropdown.Item>
+          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
